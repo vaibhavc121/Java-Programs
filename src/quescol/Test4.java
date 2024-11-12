c110688e8ce8e877e6c35cfaaee6070d88208aae
vaibhav <vaibhavc121@gmail.com>
2024-11-12 23:02:26 +0530

    12-11

---
+++ src/quescol/Test4.java
@@ -0,0 +1,21 @@
+package quescol;
+
+public class Test4
+{
+	public void run()
+	{
+
+		System.out.println("vaibhav");
+	}
+
+	int a;
+
+	public static void main(String[] args)
+	{
+		Test4 ob = new Test4();
+		ob.run();
+		ob.a = 10;
+		System.out.println(ob.a);
+	}
+
+}
