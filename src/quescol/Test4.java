c5ed394a9f8181983d9ccdebae72bb5c8fae4840
vaibhav <vaibhavc121@gmail.com>
2024-11-22 22:54:58 +0530

    22-11

--- src/quescol/Test4.java
+++ src/quescol/Test4.java
@@ -4,7 +4,10 @@ public class Test4
 {
 	public void run()
 	{
+		Test5 t = new Test5();
+		t.a = 1;
 
+		System.out.println(t.a);
 		System.out.println("vaibhav");
 	}
 
@@ -13,6 +16,7 @@ public class Test4
 	public static void main(String[] args)
 	{
 		Test4 ob = new Test4();
+
 		ob.run();
 		ob.a = 10;
 		System.out.println(ob.a);
