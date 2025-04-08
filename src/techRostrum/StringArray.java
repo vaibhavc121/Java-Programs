7753a5b72f4660a8660d1de837ae7ef6eb07c83d
vaibhav <vaibhavc121@gmail.com>
2025-04-08 23:34:50 +0530

    8-4

--- src/techRostrum/StringArray.java
+++ src/techRostrum/StringArray.java
@@ -6,25 +6,32 @@ public class StringArray
 	public static void main(String[] args)
 	{
 		int[] arr =
-		{ 4, 1, 3, 2, 5 };
+		{ 1, 2, 3, 4, 5 };
 
-		for (int i = 0; i < arr.length - 1; i++)
+		for (int x : arr)
 		{
-			for (int j = i + 1; j < arr.length; j++)
-			{
-				if (arr[i] > arr[j])
-				{
-					// Swap arr[i] and arr[j]
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
+			System.out.print(x + ",");
 		}
-		for (int i = 0; i < arr.length; i++)
-		{
-			System.out.println(arr[i]);
-		}
+//		int[] arr =
+//		{ 4, 1, 3, 2, 5 };
+//
+//		for (int i = 0; i < arr.length - 1; i++)
+//		{
+//			for (int j = i + 1; j < arr.length; j++)
+//			{
+//				if (arr[i] > arr[j])
+//				{
+//					// Swap arr[i] and arr[j]
+//					int temp = arr[i];
+//					arr[i] = arr[j];
+//					arr[j] = temp;
+//				}
+//			}
+//		}
+//		for (int i = 0; i < arr.length; i++)
+//		{
+//			System.out.println(arr[i]);
+//		}
 
 //		int[] arr =
 //		{ 4, 1, 3, 2, 5 };
